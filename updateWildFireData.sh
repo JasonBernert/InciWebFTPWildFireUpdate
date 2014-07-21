9118d6d789e949a7036b203836c03edeeb8405e1
Jason Bernert <jasonbernert@gmail.com>
2014-07-21 10:53:16 -0700

    Create updateWildFireData.sh

---
+++ updateWildFireData.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+wget -i wildfireDownloadList.txt -P /Users/jbernert/Documents/Projects/orFireMap/liveORfireMap/autoUpdate/nwcc_ftp_shapefiles
+cd nwcc_ftp_shapefiles/
+DATE=$(date +%Y%m%d%H)
+mv "NWCC_POINT_DAILY.geojson" "NWCC_POINT_DAILY"_$DATE.geojson
+mv "NWCC_POLY_DAILY.geojson" "NWCC_POLY_DAILY"_$DATE.geojson
+ogr2ogr -f GeoJSON -t_srs crs:84 NWCC_POINT_DAILY.geojson NWCC_POINT_DAILY.shp
+ogr2ogr -f GeoJSON -t_srs crs:84 NWCC_POLY_DAILY.geojson NWCC_POLY_DAILY.shp
+echo 'var fires = ' | cat - NWCC_POINT_DAILY.geojson > temp && mv temp NWCC_POINT_DAILY.geojson
+echo 'var perimeters = ' | cat - NWCC_POLY_DAILY.geojson > temp && mv temp NWCC_POLY_DAILY.geojson
+rm $(<../removeShapefiles.txt)
